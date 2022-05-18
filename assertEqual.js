453c980cd5ae09951d944c58c46c2fe82d01689e
Hope Warren <hopevaughnwarren@gmail.com>
2022-05-18 10:24:19 -0700

    Colourful replacement for the assert method

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(
+      `🟢🟢🟢 Asstertion Passed: [${actual}] === [${expected}]`
+    );
+  } else {
+    return console.log(
+      `🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`
+    );
+  }
+};
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('hey bud', 'hey bud');
+assertEqual('hey bud', 'hey buds');
+assertEqual(42, 42);
+assertEqual(13, 31);
