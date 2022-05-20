ccbf3548b2f18b78d6a6994debb13fa800d367f7
Hope Warren <hopevaughnwarren@gmail.com>
2022-05-19 17:58:31 -0700

    refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,11 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(
-      `🟢🟢🟢 Asstertion Passed: [${actual}] === [${expected}]`
-    );
-  } else {
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
     return console.log(
       `🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`
     );
   }
+  return console.log(`🟢🟢🟢 Asstertion Passed: [${actual}] === [${expected}]`);
 };
 
 // TEST CODE
