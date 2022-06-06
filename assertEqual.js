df74a3706a201ea2b43fc2546415ee55d738a056
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-06 09:06:18 -0700

    Updated code for export and added test folder. linted

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,9 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual !== expected) {
     return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
   return console.log(`🟢🟢🟢 Asstertion Passed: ${actual} === ${expected}`);
 };
 
-// TEST CODE
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('hey bud', 'hey bud');
-assertEqual('hey bud', 'hey buds');
-assertEqual(42, 42);
-assertEqual(13, 31);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  return console.log(`🟢🟢🟢 Asstertion Passed: ${actual} === ${expected}`);
-};
+const assertEqual = require('./assertEqual')
 
-const head = function(array) {
+const head = function (array) {
   const value = array[0];
   if (value === undefined) {
     return;
@@ -14,7 +9,6 @@ const head = function(array) {
   return value;
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([3]), 3);
-assertEqual(head([]), 5);
+
+module.exports = head
+
