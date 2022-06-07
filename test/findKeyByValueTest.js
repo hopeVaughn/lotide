a16bd5b06acfc6cf9b4f9212f687dc6fa6f837a4
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-07 13:43:00 -0700

    Delete test directory
    not needed

--- test/findKeyByValueTest.js
+++
@@ -1,9 +0,0 @@
-// const bestTVShowsByGenre = {
-//   sci_fi: 'The Expanse',
-//   comedy: 'Brooklyn Nine-Nine',
-//   drama: 'The Wire',
-// };
-
-// console.log(findKeyByValue(bestTVShowsByGenre, 'The Wire'));
-// assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
--- test/letterPositionTest.js
+++
@@ -1,6 +0,0 @@
-// ---------------------------------------------------------------------
-// Test cases
-// console.log(letterPositions('aa bb cc'));
-// console.log(letterPositions('hey friend hows life'));
-// assertArrayEquals(letterPositions('hey friend hows life').f, [4, 18]);
-// assertArrayEquals(letterPositions('aa bb cc').a, [0, 1]);
